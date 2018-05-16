0ec7f21ff0b66a52f9bb38f9a73741773cdc4e1d
Naim Mustafa <1dakikabooya@gmail.com>
2018-05-16 12:02:02 +0100

    another button

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -9,3 +9,18 @@ function sayHello() {
 function refreshPage() {
     location.reload();
 }
+
+function checkRes() {
+    var x, text;
+
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("numb").value;
+
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x != 7) {
+        text = "Try Again";
+    } else {
+        text = "Correct!";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
